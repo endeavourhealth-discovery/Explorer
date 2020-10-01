32e01685bb934d59bc54f279ce0a8564fad1c515
ayshui <59893536+ayshui@users.noreply.github.com>
2020-10-01 17:11:25 +0100

    initial version

--- Database/createDatasetTables.sql
+++ Database/createDatasetTables.sql
@@ -14,7 +14,7 @@ DROP TABLE IF EXISTS person_dataset;
 CREATE TABLE person_dataset (
   query_id INT(11) NOT NULL,
   patient_id BIGINT(20) NOT NULL,
-  UNIQUE KEY (patient_id)
+  PRIMARY KEY (query_id, patient_id)
 ) ENGINE=InnoDB CHARSET=latin1;
 
 DROP TABLE IF EXISTS observation_dataset;
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS observation_dataset;
 CREATE TABLE observation_dataset (
   query_id INT(11) NOT NULL,
   observation_id BIGINT(20) NOT NULL,
-  UNIQUE KEY (observation_id)
+  PRIMARY KEY (query_id, observation_id)
 ) ENGINE=InnoDB CHARSET=latin1;
 
 DROP TABLE IF EXISTS medication_dataset;
@@ -30,7 +30,7 @@ DROP TABLE IF EXISTS medication_dataset;
 CREATE TABLE medication_dataset (
   query_id INT(11) NOT NULL,
   medication_id BIGINT(20) NOT NULL,
-  UNIQUE KEY (medication_id)
+  PRIMARY KEY (query_id, medication_id)
 ) ENGINE=InnoDB CHARSET=latin1;
 
 DROP TABLE IF EXISTS encounter_dataset;
@@ -38,7 +38,7 @@ DROP TABLE IF EXISTS encounter_dataset;
 CREATE TABLE encounter_dataset (
   query_id INT(11) NOT NULL,
   encounter_id BIGINT(20) NOT NULL,
-  UNIQUE KEY (encounter_id)
+  PRIMARY KEY (query_id, encounter_id)
 ) ENGINE=InnoDB CHARSET=latin1;
 
 
