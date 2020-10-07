47c4633eb0f8e36afb1c29ddff15710af0caa1a3
ayshui <59893536+ayshui@users.noreply.github.com>
2020-10-07 17:52:04 +0100

    initial version

--- Database/createEventScheduler.sql
+++ Database/createEventScheduler.sql
@@ -2,9 +2,9 @@ USE dashboards;
 
 DELIMITER //
 
-DROP EVENT IF EXISTS `runReportGeneratorEvery1Minute`;
+DROP EVENT IF EXISTS `runReportGeneratorEveryMinute`;
 
-CREATE EVENT `runReportGeneratorEvery1Minute`
+CREATE EVENT `runReportGeneratorEveryMinute`
 ON SCHEDULE EVERY 1 MINUTE STARTS '2020-10-01 00:00:00'
 ON COMPLETION PRESERVE
 DO BEGIN
