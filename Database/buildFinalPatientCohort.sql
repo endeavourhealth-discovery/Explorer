26ef756329875a92e9b5827268a104e8a8a8a88f
ayshui <59893536+ayshui@users.noreply.github.com>
2020-10-14 16:45:28 +0100

    initial version

--- Database/buildFinalPatientCohort.sql
+++ Database/buildFinalPatientCohort.sql
@@ -20,28 +20,174 @@ CREATE PROCEDURE buildFinalPatientCohort (
 
 BEGIN
 
-    SET @sql = CONCAT('DROP TABLE IF EXISTS ',p_patientcohorttab);
+    DECLARE qrytabname VARCHAR(64);
+
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp;
+    SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp AS 
+    SELECT DISTINCT ', p_query_id,' AS query_id, o.patient_id 
+    FROM ', p_observationcohorttab,' o ');
+    PREPARE stmt FROM @sql;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+    ALTER TABLE qry_tmp ADD INDEX pat_idx(patient_id);
+
+    SET qrytabname = 'qry_tmp';
+-- 1
+    IF p_includeExclude1String <> '1' THEN
+
+      DROP TEMPORARY TABLE IF EXISTS qry_tmp_1;
+      SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp_1 AS 
+      SELECT DISTINCT o.query_id, o.patient_id 
+      FROM ', qrytabname,' o 
+      WHERE ', p_includeExclude1String);
+      PREPARE stmt FROM @sql;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      ALTER TABLE qry_tmp_1 ADD INDEX pat_idx(patient_id);
+
+      SET qrytabname = 'qry_tmp_1';
+
+    END IF;
+-- 1a
+    IF p_includeExclude1aString <> '1' THEN
+
+      DROP TEMPORARY TABLE IF EXISTS qry_tmp_1a;
+      SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp_1a AS 
+      SELECT DISTINCT o.query_id, o.patient_id 
+      FROM ', qrytabname,' o 
+      WHERE ', p_includeExclude1aString);
+      PREPARE stmt FROM @sql;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      ALTER TABLE qry_tmp_1a ADD INDEX pat_idx(patient_id);
+
+      SET qrytabname = 'qry_tmp_1a';
+
+    END IF;
+-- 1b
+    IF p_includeExclude1bString <> '1' THEN
+
+      DROP TEMPORARY TABLE IF EXISTS qry_tmp_1b;
+      SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp_1b AS 
+      SELECT DISTINCT o.query_id, o.patient_id 
+      FROM ', qrytabname,' o 
+      WHERE ', p_includeExclude1bString);
+      PREPARE stmt FROM @sql;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      ALTER TABLE qry_tmp_1b ADD INDEX pat_idx(patient_id);
+
+      SET qrytabname = 'qry_tmp_1b';
+
+    END IF;
+-- 2
+    IF p_includeExclude2String <> '1' THEN
+
+      DROP TEMPORARY TABLE IF EXISTS qry_tmp_2;
+      SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp_2 AS 
+      SELECT DISTINCT o.query_id, o.patient_id 
+      FROM ', qrytabname,' o 
+      WHERE ', p_includeExclude2String);
+      PREPARE stmt FROM @sql;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      ALTER TABLE qry_tmp_2 ADD INDEX pat_idx(patient_id);
+
+      SET qrytabname = 'qry_tmp_2';
+
+    END IF;
+-- 2a    
+    IF p_includeExclude2aString <> '1' THEN
+
+      DROP TEMPORARY TABLE IF EXISTS qry_tmp_2a;
+      SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp_2a AS 
+      SELECT DISTINCT o.query_id, o.patient_id 
+      FROM ', qrytabname,' o 
+      WHERE ', p_includeExclude2aString);
+      PREPARE stmt FROM @sql;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      ALTER TABLE qry_tmp_2a ADD INDEX pat_idx(patient_id);
+
+      SET qrytabname = 'qry_tmp_2a';
+
+    END IF;
+-- 3
+    IF p_includeExclude3String <> '1' THEN
+
+      DROP TEMPORARY TABLE IF EXISTS qry_tmp_3;
+      SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp_3 AS 
+      SELECT DISTINCT o.query_id, o.patient_id 
+      FROM ', qrytabname,' o 
+      WHERE ', p_includeExclude3String);
+      PREPARE stmt FROM @sql;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      ALTER TABLE qry_tmp_3 ADD INDEX pat_idx(patient_id);
+
+      SET qrytabname = 'qry_tmp_3';
+
+    END IF;
+-- 4
+    IF p_includeExclude4String <> '1' THEN
+
+      DROP TEMPORARY TABLE IF EXISTS qry_tmp_4;
+      SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp_4 AS 
+      SELECT DISTINCT o.query_id, o.patient_id 
+      FROM ', qrytabname,' o 
+      WHERE ', p_includeExclude4String);
+      PREPARE stmt FROM @sql;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      ALTER TABLE qry_tmp_4 ADD INDEX pat_idx(patient_id);
+
+      SET qrytabname = 'qry_tmp_4';
+
+    END IF;
+-- 5
+    IF p_includeExclude5String <> '1' THEN
+
+      DROP TEMPORARY TABLE IF EXISTS qry_tmp_5;
+      SET @sql = CONCAT('CREATE TEMPORARY TABLE qry_tmp_5 AS 
+      SELECT DISTINCT o.query_id, o.patient_id 
+      FROM ', qrytabname,' o 
+      WHERE ', p_includeExclude5String);
+      PREPARE stmt FROM @sql;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      ALTER TABLE qry_tmp_5 ADD INDEX pat_idx(patient_id);
+
+      SET qrytabname = 'qry_tmp_5';
+
+    END IF;
+    
+-- build patient cohort table
+
+    SET @sql = CONCAT('DROP TABLE IF EXISTS ', p_patientcohorttab);
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-    SET @sql = CONCAT('CREATE TABLE ',p_patientcohorttab,' AS 
-    SELECT DISTINCT ',p_query_id ,' AS query_id, o.patient_id
-    FROM ',p_observationcohorttab ,' o  
-    WHERE ',p_includeExclude1String ,' AND ',p_includeExclude1aString ,
-    ' AND ',p_includeExclude1bString ,' AND ',p_includeExclude2String ,
-    ' AND ',p_includeExclude2aString ,' AND ',p_includeExclude3String ,
-    ' AND ',p_includeExclude4String ,' AND ',p_includeExclude5String);
-
+    SET @sql = CONCAT('CREATE TABLE ', p_patientcohorttab,' AS 
+    SELECT DISTINCT o.query_id, o.patient_id 
+    FROM ', qrytabname,' o ');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-    SET @sql = CONCAT('ALTER TABLE ',p_patientcohorttab ,' ADD INDEX pat_idx(patient_id)');
+    SET @sql = CONCAT('ALTER TABLE ', p_patientcohorttab,' ADD INDEX pat_idx(patient_id)');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp_1;
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp_1a;
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp_1b;
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp_2;
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp_2a;
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp_3;
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp_4;
+    DROP TEMPORARY TABLE IF EXISTS qry_tmp_5;
 
 END//
 DELIMITER ;
