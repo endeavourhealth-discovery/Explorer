513b6a444dede1bcae6a223c497e6bbf4ec06386
ayshui <59893536+ayshui@users.noreply.github.com>
2020-10-14 11:03:40 +0100

    initial version

--- Database/createPatientCohort.sql
+++ Database/createPatientCohort.sql
@@ -30,32 +30,37 @@ BEGIN
 
    SET @sql = CONCAT('CREATE TABLE ',p_cohorttab, 
    ' AS SELECT DISTINCT 
-          e.person_id,
-          e.patient_id,
-          e.date_registered,
-          e.date_registered_end,
-          org.name AS organization_name,
-          org.id AS organization_id,
-          org.ods_code,
-          p.nhs_number,
-          p.date_of_birth,
-          p.date_of_death,
-          IF(p.date_of_death IS NULL, FLOOR(DATEDIFF(NOW(), p.date_of_birth) / 365.25), FLOOR(DATEDIFF(p.date_of_death, p.date_of_birth) / 365.25)) AS age,
-          c2.code AS gender,
-          pa.postcode
+          e.person_id, 
+          e.patient_id, 
+          e.date_registered, 
+          e.date_registered_end, 
+          org.name AS organization_name, 
+          org.id AS organization_id, 
+          org.ods_code, 
+          p.nhs_number, 
+          p.date_of_birth, 
+          p.date_of_death, 
+          IF(p.date_of_death IS NULL, FLOOR(DATEDIFF(NOW(), p.date_of_birth) / 365.25), FLOOR(DATEDIFF(p.date_of_death, p.date_of_birth) / 365.25)) AS age, 
+          c2.code AS gender, 
+          pa.postcode 
    FROM ',p_schema,'.episode_of_care e 
    JOIN ',p_schema,'.organization org on org.id = e.organization_id 
-   JOIN ',p_schema,'.concept c ON c.dbid = e.registration_type_concept_id
-   JOIN ',p_schema,'.patient p ON p.id = e.patient_id
-   JOIN ',p_schema,'.concept c2 ON c2.dbid = p.gender_concept_id
-   LEFT JOIN ',p_schema,'.patient_address pa ON p.current_address_id = pa.id AND p.id = pa.patient_id
-   WHERE ',p_regstatus,
+   JOIN ',p_schema,'.concept c ON c.dbid = e.registration_type_concept_id 
+   JOIN ',p_schema,'.patient p ON p.id = e.patient_id 
+   JOIN ',p_schema,'.concept c2 ON c2.dbid = p.gender_concept_id 
+   LEFT JOIN ',p_schema,'.patient_address pa ON p.current_address_id = pa.id AND p.id = pa.patient_id 
+   WHERE ',p_regstatus, 
    ' AND e.id >= (SELECT MAX(e2.id) 
-                  FROM ',p_schema,'.episode_of_care e2
+                  FROM ',p_schema,'.episode_of_care e2 
                   JOIN ',p_schema,'.organization org2 ON org2.id = e2.organization_id 
-			            JOIN ',p_schema,'.concept c ON c.dbid = e2.registration_type_concept_id
-                  WHERE ',p_regstatus,' AND e2.person_id = e.person_id) 
-     AND ',p_org,' AND ',p_agerange,' AND ',p_genderrange,' AND ',p_postcoderange,' AND ',p_death);
+                  JOIN ',p_schema,'.concept c ON c.dbid = e2.registration_type_concept_id 
+                  WHERE ',p_regstatus,' 
+                  AND e2.person_id = e.person_id) 
+     AND ',p_org,' 
+     AND ',p_agerange,' 
+     AND ',p_genderrange,' 
+     AND ',p_postcoderange,' 
+     AND ',p_death);
      
    PREPARE stmt FROM @sql;
    EXECUTE stmt;
