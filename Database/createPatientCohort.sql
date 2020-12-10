1264d861b6e19fe5a2f31fd16724de1d178e3e40
ayshui <59893536+ayshui@users.noreply.github.com>
2020-12-10 10:23:28 +0000

    fixed reg status

--- Database/createPatientCohort.sql
+++ Database/createPatientCohort.sql
@@ -19,14 +19,22 @@ BEGIN
 
    DECLARE p_death VARCHAR(100) DEFAULT NULL;
    DECLARE where_clause_1 VARCHAR(1000) DEFAULT NULL;
+   DECLARE regstatus_1 VARCHAR(255) DEFAULT NULL;
+   DECLARE regstatus_2 VARCHAR(255) DEFAULT NULL;
+
 
    SET p_regIncludeExclude = UPPER(p_regIncludeExclude);
    SET p_regIncludeExclude = IF(p_regIncludeExclude = 'EXCLUDE','NOT EXISTS', IF(p_regIncludeExclude = 'INCLUDE','EXISTS',''));
 
    IF p_regstatus <> '1' THEN
     SET p_death = 'p.date_of_death IS NULL';
+    SET regstatus_1 = p_regstatus;
+    SET regstatus_2 = REPLACE(p_regstatus,'e.','e2.');
+    SET regstatus_2 = REPLACE(regstatus_2,'c.','c3.');
    ELSE
     SET p_death = '1';
+    SET regstatus_1 = '1';
+    SET regstatus_2 = '1';
    END IF;
 
    DROP TEMPORARY TABLE IF EXISTS qry_tmp;
@@ -52,13 +60,13 @@ BEGIN
    JOIN ',p_schema,'.patient p ON p.id = e.patient_id 
    JOIN ',p_schema,'.concept c2 ON c2.dbid = p.gender_concept_id 
    LEFT JOIN ',p_schema,'.patient_address pa ON p.current_address_id = pa.id AND p.id = pa.patient_id 
-   WHERE ',p_regstatus, 
+   WHERE ',regstatus_1, 
    ' AND e.id >= (SELECT MAX(e2.id) 
                   FROM ',p_schema,'.episode_of_care e2 
                   JOIN ',p_schema,'.organization org2 ON org2.id = e2.organization_id 
-                  JOIN ',p_schema,'.concept c ON c.dbid = e2.registration_type_concept_id 
-                  WHERE ',p_regstatus,' 
-                  AND e2.person_id = e.person_id) 
+                  JOIN ',p_schema,'.concept c3 ON c3.dbid = e2.registration_type_concept_id 
+                  WHERE ',regstatus_2,' 
+                  AND e2.person_id = e.person_id AND e2.organization_id = e.organization_id) 
      AND ',p_org,' 
      AND ',p_agerange,' 
      AND ',p_genderrange,' 
